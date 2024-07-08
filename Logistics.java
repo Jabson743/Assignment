3c2739141514bffe21396cbc53ac64674e77ab8b
Jabson743 <138734239+Jabson743@users.noreply.github.com>
2024-07-07 16:58:58 -1200

    second commit

---
+++ Logistics.java
@@ -0,0 +1,24 @@
+public class Logistics {
+
+      public static int multiply (int firstNumber, int secondNumber) {
+      
+      int multiply = firstNumber * secondNumber;
+
+      int total = multiply + 5000;
+
+      return total;
+
+    }
+
+      public static int multiplication (int firstNumber, int secondNumber) {
+
+      int multiply = firstNumber * secondNumber;
+
+      int total = multiply + 5000;
+
+      return total;
+
+    } 
+
+
+}
---
+++ LogisticsService.java
@@ -0,0 +1,9 @@
+public class LogisticsService {
+
+       public static void main(String[] args) {
+ 
+       System.out.println(Logistics.multiply(80, 500));
+       System.out.println(Logistics.multiplication(25, 160));
+
+ }
+}
