1e0f59276899a4a582fc3dd089df427cb6e88af7
Jabson743 <138734239+Jabson743@users.noreply.github.com>
2024-08-05 18:52:39 -1200

    newest commit

--- gateOne/MBTI/PersonalityTest.java
+++ gateOne/MBTI/PersonalityTest.java
@@ -8,8 +8,10 @@ public class PersonalityTest {
        System.out.print("What is your name: ");
        String name = input.nextLine();
        
-       System.out.print("The questions are from 1 to 20 in total: ");
+       System.out.print("Press 1 to start: ");
        int number = input.nextInt();
+
+       for(int counter = 1; counter <= 20; counter++) {
 
        switch (number) {
 
@@ -86,6 +88,41 @@ public class PersonalityTest {
        System.out.println("Invalid option. Please select 'A' or 'B'.");
        } 
 
+       case 5: 
+       System.out.println("Qusetion 5. How do you feel about routine and predictability?");
+       System.out.println("A. Prefer routine and find comfort in predictability");
+       System.out.println("B. Dislike routine and enjoy spontaneity");
+
+       String answer5 = input.nextLine();
+
+       if (answer5.equals("A")) {
+       System.out.println("Prefer routine and find comfort in predictability");
+       }
+       else if (answer5.equals("B")) {
+       System.out.println("Dislike routine and enjoy spontaneity");
+       } 
+       else {
+       System.out.println("Invalid option. Please select 'A' or 'B'.");
+       } 
+
+       case 6:
+       System.out.println("Qusetion 6. How do you receive information?");
+       System.out.println("A. Clear and straightforward explanations");
+       System.out.println("B. Rich with possibilities and potentials connections");
+
+       String answer6 = input.nextLine();
+
+       if (answer6.equals("A")) {
+       System.out.println("Clear and straightforward explanations");
+       }
+       else if (answer6.equals("B")) {
+       System.out.println("Rich with possibilities and potentials connections");
+       } 
+       else {
+       System.out.println("Invalid option. Please select 'A' or 'B'.");
+       } 
+
+     }
     }
   }
 }
--- gateOne/MBTI/personalitytest.py
+++ gateOne/MBTI/personalitytest.py
@@ -2,43 +2,143 @@ print("Welcome To Semicolon MBTI Personality Test.")
 
 name = input("What is your name: ")
 
-number = int(input("Enter a number from 1 to 20"))
+number = int(input("Press 1 to start: "))
 
-match(number):
+for number in range(1, 21):
 
-	case 1:
-		print("Question 1.")
-		print("How do you approach deadlines?")
-		print("A. Work diligently to meet deadlines well in advance")
-		print("B. Tend to work better under pressure and close to the deadlines")
+	match(number):
 
-		answer1 = input()
-
-		if(answer1 == "A"):
+		case 1:
+			print("Question 1.")
+			print("How do you approach deadlines?")
 			print("A. Work diligently to meet deadlines well in advance")
+			print("B. Tend to work better under pressure and close to the deadlines")
 
-		elif(answer1 == "B"):
-			print("Tend to work better under pressure and close to the deadlines")
+			answer1 = input()
 
-		else:
-			print("Invalid option. Please select 'A' or 'B'.")
+			if(answer1 == "A"):
+				print("A. Work diligently to meet deadlines well in advance")
 
-	case 2:
-		print("Question 2.")
-		print("In a group of discussion, do you prefer to stick to the facts and details or contribute ideas and theories?")
-		print("A. Stick to facts and details")
-		print("B. Contribute ideas and theories")
-		
-		answer2 = input()
+			elif(answer1 == "B"):
+				print("Tend to work better under pressure and close to the deadlines")
 
-		if(answer2 == "A"):
+			else:
+				print("Invalid option. Please select 'A' or 'B'.")
+
+		case 2:
+			print("Question 2.")
+			print("In a group of discussion, do you prefer to stick to the facts and details or contribute ideas and theories?")
 			print("A. Stick to facts and details")
-
-		elif(answer2 == "B"):
 			print("B. Contribute ideas and theories")
-
-		else:
-			print("Invalid option. Please select 'A' or 'B'.")
-
-
 		
+			answer2 = input()
+
+			if(answer2 == "A"):
+				print("A. Stick to facts and details")
+
+			elif(answer2 == "B"):
+				print("B. Contribute ideas and theories")
+
+			else:
+				print("Invalid option. Please select 'A' or 'B'.")
+
+		case 3:
+			print("Question 3.")
+			print("When planning a weekend, do you lean towards social plans with friends or quiet time for yourself?")
+			print("A. Social plans with friends")
+			print("B. Quiet time for yourself")
+
+			answer3 = input()
+
+			if(answer3 == "A"):
+				print("A. Social plans with friends")
+
+			elif(answer2 == "B"):
+				print("B. Quiet time for yourself")
+
+			else:
+				print("Invalid option. Please select 'A' or 'B'.")
+
+		case 4:
+			print("Question 4")
+			print("How do you feel about small tasks?")
+			print("A. Enjoy it and find it easy to engage in")
+			print("B. Find it somewhat akward or draining")
+
+			answer4 = input()
+
+			if(answer4 == "A"):
+				print("A. Enjoy it and find it easy to engage in")
+
+			elif(answer4 == "B"):
+				print("B. Find it somewhat akward or draining")
+
+			else:
+				print("Invalid option. Please select 'A' or 'B'.")
+
+
+		case 5: 
+			print("Question 5")
+			print("How do you feel about routine and predictability?")
+			print("A. Prefer routine and find comfort in predictability")
+			print("B. Dislike routine and enjoy spontaneity")
+
+			answer5 = input()
+
+			if(answer5 == "A"):
+				print("A. Prefer routine and find comfort in predictability")
+			
+			elif(answer5 == "B"):
+				print("B. Dislike routine and enjoy spontaneity")
+
+			else:
+				print("Invalid option. Please select 'A' or 'B'.")
+
+		case 6:
+			print("Question 6")
+			print("How do you receive information?")
+			print("A. Clear and straightforward explanations")
+			print("B. Rich with possibilities and potentials connections")
+
+			answer6 = input()
+
+			if(answer6 == "A"):
+				print("A. Clear and straightforward explanations")
+			
+			elif(answer5 == "B"):
+				print("B. Rich with possibilities and potentials connections")
+
+			else:
+				print("Invalid option. Please select 'A' or 'B'.")
+
+		case 7:
+			print("Question 7")
+			print("How do handle new and unfamiliar situations?")
+			print("A. Embrace them with enthusiams")
+			print("B. Approach them with caution")
+
+			answer7 = input()
+
+			if(answer7 == "A"):
+				print("A. Embrace them with enthusiams")
+			
+			elif(answer5 == "B"):
+				print("B. Approach them with caution")
+
+			else:
+				print("Invalid option. Please select 'A' or 'B'.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
